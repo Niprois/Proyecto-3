86812365188c56f87e89ad0c2ec4051f8a8adea8
Niprois <alvarof628@gmail.com>
2023-12-11 00:42:50 -0300

    Creado

---
+++ Proyecto3.cpp
@@ -0,0 +1,18 @@
+#ifdef _WIN32
+#include <Windows.h>
+#else
+#include <unistd.h>
+#endif
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+#include <bits/stdc++.h>
+#include <random>
+
+using namespace std;
+
+int main()
+{
+
+    
+}
