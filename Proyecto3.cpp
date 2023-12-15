7571c502b42f1223fefea485310d41e657587490
Niprois <alvarof628@gmail.com>
2023-12-14 22:14:16 -0300

    Ultimate

--- Proyecto3.cpp
+++ Proyecto3.cpp
@@ -8,8 +8,8 @@
 using namespace std;
 
 void checkAndChange(int elemCount, int Numero[]) {
-    // Seed for random number generation
-    std::srand(std::time(0));
+    
+    srand(time(0));
 
     for (int i = 0; i < elemCount; ++i) {
         for (int j = i + 1; j < elemCount; ++j) {
@@ -18,9 +18,6 @@ void checkAndChange(int elemCount, int Numero[]) {
             }
         }
     }
-}
-void crearnewArr(int Numero[]){
-    
 }
 
 
@@ -92,48 +89,219 @@ void shellSort(int elemtcount, int Numero[]){
 
 }
 
-void mergeSort(int Numero[], int inicio, int fin){
-    if (inicio < fin)
-    {
+void merge(int Numero[], int left, int mid, int right) {
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
 
+    int L[n1], R[n2];
+
+    for (int i = 0; i < n1; i++)
+        L[i] = Numero[left + i];
+    for (int j = 0; j < n2; j++)
+        R[j] = Numero[mid + 1 + j];
+
+
+    int i = 0;     
+    int j = 0;     
+    int k = left;  
+
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            Numero[k] = L[i];
+            i++;
+        } else {
+            Numero[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1) {
+        Numero[k] = L[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2) {
+        Numero[k] = R[j];
+        j++;
+        k++;
     }
 }
 
-void quickSort(int elemtcount, int Numero[]){
+//
+//
+int* mergeSort(int arr[], int left, int right) {
+    if (left < right) {
+        int mid = left + (right - left) / 2;
 
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+
+        merge(arr, left, mid, right);
+
+        return arr;
+    }
+}
+int partition(int inArray[], int low, int high){
+    int i = low-1;
+    int pivot = inArray[high];
+
+    for (int j=low; j < high; j++){
+        if (inArray[j] <= pivot){
+            i++;
+            int temp = inArray[i];
+            inArray[i] = inArray[j];
+            inArray[j] = temp;
+        }
+    }
+
+    int temp = inArray[i+1];
+    inArray[i+1] = inArray[high];
+    inArray[high] = temp;
+
+    return i+1;
 }
 
-void heapSort(int elemtcount, int Numero[]){
+//
+//
+//- quickSort(array, 0, size-1)
+void quickSort(int inArray[], int low, int high){
+    if (low < high){
+        //divide el array y obtiene un pivote
+        int pivot_index = partition(inArray, low, high);
 
+        //ordena recursivamente los subarreglos de antes y despues del pivote
+        quickSort(inArray, low, pivot_index-1);
+        quickSort(inArray, pivot_index+1, high);
+    }
+}
+
+void heapify(int inArray[], int n, int i){
+    int largest = i;
+    int left = 2*i+1;
+    int right = 2*i+2;
+
+    if (left < n && inArray[left] > inArray[largest]){
+        largest = left;
+    }
+
+    if (right < n && inArray[right] > inArray[largest]) {
+        largest = right;
+    }
+
+    if (largest != i) {
+        int temp = inArray[i];
+        inArray[i] = inArray[largest];
+        inArray[largest] = temp;
+
+        heapify(inArray, n, largest);
+    }
+}
+
+void heapSort(int inArray[], int size){
+    int s = size;
+    
+    for (int i=(s/2)-1; i>=0; i--){
+        heapify(inArray, s, i);
+    }
+
+    for (int i=s-1; i>=0; i--) {
+        int temp = inArray[0];
+        inArray[0] = inArray[i];
+        inArray[i] = temp;
+
+        heapify(inArray, i, 0);
+    }
+}
+void copyArray(const int source[], int destination[], int size) {
+    for (int i = 0; i < size; ++i) {
+        destination[i] = source[i];
+    }
 }
 int main()
 { 
     srand((time(NULL)));
-    int limiteIntervalo = rand()%(110000 - 100000 + 1) + 100000;
-    int menu = 1;
-    int select;
+    int loopead = 1;
+    int select, Change;
+    int limiteIntervalo = 0;
+    cout << "Seleccione tamanio del arreglo: ";
+    cin >> Change;
+    if (Change == 1)
+    {
+        limiteIntervalo = rand()%(110000 - 100000 + 1) + 100000;
+    }
+    else if(Change == 2){
+        limiteIntervalo = rand()%(22501 - 15000) + 15000;
+    }
+    else if (Change == 3){
+        limiteIntervalo = rand()%(80001 - 60000) + 60000;
+    }else {
+        limiteIntervalo = rand()%(22501 - 15000) + 15000;        
+    }
+    int carready;
     int Numero[limiteIntervalo];
-    int ArrBubble[limiteIntervalo];
+    int PhNumero[limiteIntervalo];
     int i,j,aux;
     size_t size = sizeof(Numero);
     size_t elemtcount = size / sizeof(Numero[0]);
-
 
     cout << "Tamanio de arreglo: " << elemtcount << endl;
     // Generate and print random values for the array
     cout << "Asignando valores al arreglo";
     for (int i = 0; i < elemtcount; ++i) {
         Numero[i] = rand() % 1000000000 + 1;
-    }
+    }    
+            auto start = chrono::high_resolution_clock::now();
+            auto end = chrono::high_resolution_clock::now();
+            auto duration = chrono::duration_cast<chrono::microseconds>(end - start);    
+    copyArray(Numero,PhNumero,elemtcount);
 
-    cout << endl;
+        cout << "Empezar carrera." << endl;
+        start = chrono::high_resolution_clock::now();
+        insertionSort(elemtcount,PhNumero);
+        end = chrono::high_resolution_clock::now();
+        duration = chrono::duration_cast<chrono::microseconds>(end - start);
+        cout << "\nTiempo transcurrido por insertion Sort es " << duration.count() << " microseconds." << endl;
+        copyArray(Numero,PhNumero,elemtcount);
 
-    auto start = chrono::high_resolution_clock::now();
-    shellSort(elemtcount,Numero);
-    auto end = chrono::high_resolution_clock::now();
-    auto duration = chrono::duration_cast<chrono::microseconds>(end - start);
-    cout << "\nTiempo transcurrido por Shell Sort es " << duration.count() << " microseconds." << endl;
+        start = chrono::high_resolution_clock::now();
+        selectionSort(elemtcount,PhNumero);
+        end = chrono::high_resolution_clock::now();
+        duration = chrono::duration_cast<chrono::microseconds>(end - start);
+        cout << "\nTiempo transcurrido por selection Sort es " << duration.count() << " microseconds." << endl;
+        copyArray(Numero,PhNumero,elemtcount);
 
+        start = chrono::high_resolution_clock::now();
+        shellSort(elemtcount,PhNumero);
+        end = chrono::high_resolution_clock::now();
+        duration = chrono::duration_cast<chrono::microseconds>(end - start);
+        cout << "\nTiempo transcurrido por shell Sort es " << duration.count() << " microseconds." << endl;
+        copyArray(Numero,PhNumero,elemtcount);          
+
+        start = chrono::high_resolution_clock::now();
+        mergeSort(PhNumero,0, elemtcount-1);
+        end = chrono::high_resolution_clock::now();
+        duration = chrono::duration_cast<chrono::microseconds>(end - start);
+        cout << "\nTiempo transcurrido por merge Sort es " << duration.count() << " microseconds." << endl;
+        copyArray(Numero,PhNumero,elemtcount);                                  
+
+        start = chrono::high_resolution_clock::now();
+        quickSort(PhNumero,0, elemtcount-1);
+        end = chrono::high_resolution_clock::now();
+        duration = chrono::duration_cast<chrono::microseconds>(end - start);
+        cout << "\nTiempo transcurrido por quick Sort es " << duration.count() << " microseconds." << endl;
+        copyArray(Numero,PhNumero,elemtcount);          
+
+        start = chrono::high_resolution_clock::now();
+        heapSort(PhNumero, elemtcount);
+        end = chrono::high_resolution_clock::now();
+        duration = chrono::duration_cast<chrono::microseconds>(end - start);
+        cout << "\nTiempo transcurrido por heap Sort es " << duration.count() << " microseconds." << endl;      
+
+          
+
+    /*
     start = chrono::high_resolution_clock::now();
     insertionSort(elemtcount,Numero);
     end = chrono::high_resolution_clock::now();
@@ -147,7 +315,7 @@ int main()
     duration = chrono::duration_cast<chrono::microseconds>(end - start);
     
     cout << "\nTiempo transcurrido por Bubble Sort es " << duration.count() << " microseconds." << endl;     
-
+/*
     /*cout<<"Ascendente: ";
     for(i=0;i<elemtcount;i++)
     {
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# Proyecto-3
+Programa creado por Alvaro Fuentes
+Fecha: 14-12-2023
+Curso: Algoritmos y estructuras de datos
+Este programa busca demostrar los diferentes algoritmos de organización existentes, tales como el selection, bubble, insertion, shell, merge, quick y heap sort, los cuales tienen la capacidad de ordenar arreglos de maneras diferentes, donde algunos seran mas efectivos dependiendo del tamaño de dichos arreglos que se usaran para poner a prueba el programa.
